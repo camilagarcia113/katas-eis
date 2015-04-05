b564b9ae8e3eb7df8223f415bf8951a6acc4d958
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-04-05 19:43:31 -0300

    Test 6 andando, test 2 todavia comentado

--- char_counter/model/char_counter.rb
+++ char_counter/model/char_counter.rb
@@ -2,10 +2,15 @@ class CharCounter
 
   def self.count(word)
     hash = Hash.new(0)
-    word.each_char do |char|
-      next unless char =~/\w/
-      hash[char] += 1
+    if word.nil?
+      return {}
+    else
+      word.each_char do |char|
+        next unless char =~/\w/
+        hash[char] += 1
+      end
+      return hash
     end
-    hash
+
   end
 end
