817f14d5381c369c58e49e31b1d7d0f47d1e2073
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-04-22 23:30:29 -0300

    Test 2 corre

--- char_counter2/model/char_counter2.rb
+++ char_counter2/model/char_counter2.rb
@@ -1,10 +1,14 @@
 class CharCounter
 
   def self.count(word)
-    hash = Hash.new(0)
-    word.each_char do |char|
-    next unless char =~/\w/
-    hash[char] += 1
+    hash = Hash.new
+    if word.nil?
+      return {}
+    else
+      word.each_char do |char|
+        next unless char =~/\w/
+        hash[char] = word.count(char)
+        end
     end
     hash
   end
