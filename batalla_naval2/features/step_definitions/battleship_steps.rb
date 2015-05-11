5e73ccc4502bc74b5c55202ebe002a364957fefa
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-05-11 10:53:39 -0300

    Primer scenario completo

--- batalla_naval2/features/step_definitions/battleship_steps.rb
+++ batalla_naval2/features/step_definitions/battleship_steps.rb
@@ -1,15 +1,16 @@
 require_relative '../../app/models/Board.rb'
+require_relative '../../app/models/Ships.rb'
 
 
-Given(/^a board with dimensions "([^"]*)" x "([^"]*)"$/) do |xCoord, yCoord|
-  @board = Board.new xCoord, yCoord
+Given(/^a board with dimensions "(\d+)" x "(\d+)"$/) do |x, y|
+  @board = Board.new x.to_i, y.to_i
 end
 
-Given(/^I create a small ship in position "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^I create a small ship in position "(\d+):(\d+)"$/) do |x, y|
+  @board.put_ship Small_Ship.new, x.to_i, y.to_i
 end
 
-Then(/^position "([^"]*)" is not empty$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^position "(\d+):(\d+)" is not empty$/) do |x, y|
+  expect(@board.is_occupied? x.to_i, y.to_i).to eq(true)
 end
 
