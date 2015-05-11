9ba3097878fcd4dfb82f6e53863532c0cc8abd2d
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-05-11 11:28:49 -0300

    Segundo scenario completo

--- batalla_naval2/features/step_definitions/battleship_steps.rb
+++ batalla_naval2/features/step_definitions/battleship_steps.rb
@@ -7,10 +7,15 @@ Given(/^a board with dimensions "(\d+)" x "(\d+)"$/) do |x, y|
 end
 
 Given(/^I create a small ship in position "(\d+):(\d+)"$/) do |x, y|
-  @board.put_ship Small_Ship.new, x.to_i, y.to_i
+  @board.put_small_ship x.to_i, y.to_i
 end
 
 Then(/^position "(\d+):(\d+)" is not empty$/) do |x, y|
   expect(@board.is_occupied? x.to_i, y.to_i).to eq(true)
 end
+
+Given(/^I create a large ship in position "(\d+):(\d+)"$/) do |x, y|
+  @board.put_large_ship x.to_i, y.to_i, 'horizontal'
+end
+
 
