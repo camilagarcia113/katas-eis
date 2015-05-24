b06bda541906b76fdee4339f5cecf52bdf4df7e4
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-05-24 19:31:39 -0300

    Scenario terminado: dispararle a un Small_Ship

--- batalla_naval3/features/shoot.feature
+++ batalla_naval3/features/shoot.feature
@@ -3,9 +3,8 @@ Feature: Shoot
   Background:
     Given a board with dimensions "5" x "5"
     And a small ship in position: “3:3”
-    And a large ship in position: “2:2”
 
-    @wip
+
   Scenario: Shoot and hit small ship
     Given I shoot to position “3:3”
     Then I get hit
---
+++ batalla_naval3/features/step_definitions/shoot.rb
@@ -0,0 +1,15 @@
+require_relative '../../app/models/board.rb'
+require_relative '../../app/models/small_ship.rb'
+require_relative '../../app/models/large_ship.rb'
+
+Given(/^a small ship in position: “(\d+):(\d+)”$/) do |x, y|
+  @board.put_small_ship x.to_i, y.to_i
+end
+
+Given(/^I shoot to position “(\d+):(\d+)”$/) do |x, y|
+  @result = @board.shoot_to_position x.to_i, y.to_i
+end
+
+Then(/^I get hit$/) do
+  expect(@result).to eq("Ship sunk")
+end
