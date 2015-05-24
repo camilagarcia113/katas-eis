1179361acf7941cf97575fc39d4377906979c967
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-05-24 18:44:09 -0300

    Scenario terminado: poner un Large_Ship en determinada posicion

--- batalla_naval3/features/step_definitions/board_steps.rb
+++ batalla_naval3/features/step_definitions/board_steps.rb
@@ -1,5 +1,6 @@
 require_relative '../../app/models/board.rb'
 require_relative '../../app/models/small_ship.rb'
+require_relative '../../app/models/large_ship.rb'
 
 Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |x, y|
  @board = Board.new x.to_i, y.to_i
@@ -12,4 +13,9 @@ end
 Then(/^position "(\d+):(\d+)" is not empty$/) do |x, y|
  expect(@board.is_occupied? x.to_i, y.to_i).to eq(true)
 end
+
+Given(/^I create a large ship in position "(\d+):(\d+)"$/) do |x, y|
+  @board.put_large_ship x.to_i, y.to_i
+end
+
 
