58d90c11ed20dcb441aacf90b7468630b7244a66
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-05-24 19:18:03 -0300

    Refactor test dispararle a un Small_Ship

---
+++ batalla_naval3/spec/models/small_ship_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require_relative '../../app/models/board.rb'
+require_relative '../../app/models/small_ship.rb'
+require_relative '../../app/models/large_ship.rb'
+
+describe 'Small_Ship' do
+
+  before (:each) do
+    @board = Board.new 5,5
+  end
+
+  #TEST 1
+  it 'should hit the Small_Ship in position 3:3 when i shoot it' do
+    board = Board.new 5,5
+    board.put_small_ship(3, 3)
+    expect(board.shoot_to_position(3, 3)).to eq('Ship sunk')
+  end
+end
