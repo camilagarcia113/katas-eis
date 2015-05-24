f83df19da297a822c1eecb6d021e64935e6b51cc
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-05-24 18:21:33 -0300

    Scenario terminado: poner un Small_Ship en determinada posicion

--- batalla_naval3/features/step_definitions/board_steps.rb
+++ batalla_naval3/features/step_definitions/board_steps.rb
@@ -1,12 +1,15 @@
+require_relative '../../app/models/board.rb'
+require_relative '../../app/models/small_ship.rb'
+
 Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |x, y|
  @board = Board.new x.to_i, y.to_i
 end
 
-Given(/^I create a small ship in position "(.*?)"$/) do |arg1|
- pending # express the regexp above with the code you wish you had
+Given(/^I create a small ship in position "(\d+):(\d+)"$/) do |x, y|
+ @board.put_small_ship x.to_i, y.to_i
 end
 
-Then(/^position "(.*?)" is not empty$/) do |arg1|
- pending # express the regexp above with the code you wish you had
+Then(/^position "(\d+):(\d+)" is not empty$/) do |x, y|
+ expect(@board.is_occupied? x.to_i, y.to_i).to eq(true)
 end
 
