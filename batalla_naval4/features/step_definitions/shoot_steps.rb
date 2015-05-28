8d81b0b517f5eed33a2f2138ae9115de19d94155
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-05-27 23:06:57 -0300

    Errata en commit anterior, faltaban los steps y agregue vista de disparar a un Large_Ship. Ahora todos los steps estan OK

--- batalla_naval4/features/step_definitions/shoot_steps.rb
+++ batalla_naval4/features/step_definitions/shoot_steps.rb
@@ -11,19 +11,25 @@ Given(/^a small ship in position: “(\d+):(\d+)”$/) do |x, y|
 end
 
 Given(/^I shoot to position “(\d+):(\d+)”$/) do |x, y|
-  #pending
+  visit '/'
+  fill_in(:shoot_x, :with => x)
+  fill_in(:shoot_y, :with => y)
+  click_button "submit_shot"
 end
 
 Then(/^I get hit$/) do
-  #pending
+  page.should have_content("Ship sunk")
 end
 
 Given(/^a large ship in position: “(\d+):(\d+)”$/) do |x, y|
-  #pending
+  visit '/'
+  fill_in(:large_ship_x, :with => x)
+  fill_in(:large_ship_y, :with => y)
+  click_button "put_large_ship"
 end
 
 Then(/^I get damaged$/) do
-  #pending
+  page.should have_content("Ship damaged")
 end
 
 Then(/^I get water$/) do
